f4bd72b5f068c8d382e13b7dc8bfe3a7a820364b
Stephen Blais <stephen@s-blais.com>
2019-11-04 16:52:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
 # binding.pry
-    session["item"] = params[:item]
+    session[:item] = params[:item]
     @session = session
     erb :display
 
