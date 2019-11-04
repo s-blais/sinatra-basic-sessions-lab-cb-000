a47c334103f05882f7d38e8a66b2854a418e7872
Stephen Blais <stephen@s-blais.com>
2019-11-04 16:22:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
+configure do
+  enable :sessions
+  set :session_secret, "chickabean"
+end
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
 end
