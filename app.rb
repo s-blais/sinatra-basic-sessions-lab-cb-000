a0e44be053532ab92fe4ab3c3a02b0a7879ec58a
Stephen Blais <stephen@s-blais.com>
2019-11-04 17:11:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "chickabean"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :display
+  end
+
 end
